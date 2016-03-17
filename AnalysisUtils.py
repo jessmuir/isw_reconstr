fixed b0 scaling bug and <rho> fitting error

AnalysisUtils.py
@@ -114,8 +114,8 @@ def scale_Dl_byb0(inDl,b0):
     NLSS=b0.size
     Dl=inDl.copy()
     for i in xrange(NLSS):
-        Dl[:,i,:]=Dl[:,i,:]*b0[i]
-        Dl[:,:,i]=Dl[:,:,i]*b0[i]
+        Dl[:,i+1,:]=Dl[:,i+1,:]*b0[i]
+        Dl[:,:,i+1]=Dl[:,:,i+1]*b0[i]
     return Dl
 
 #================================================
@@ -281,7 +281,6 @@ def get_glm_array_forrec(glmdat,includelist=[],zerotag='isw_bin0'):
 #                also saves to file
 #-------------------------------------------------------------------------
 def calc_isw_est(cldat,glmdat,recdat,writetofile=True,getmaps=True,redofits=True,makeplots=False,dorho=False,fitbias=True):
-    
     maptag=recdat.maptag
     rectag=recdat.rectag
     lmin_forrec=recdat.lmin
@@ -309,7 +308,7 @@ def calc_isw_est(cldat,glmdat,recdat,writetofile=True,getmaps=True,redofits=True
         lmax=Nell-1
     else:
         lmax=lmax_forrec
-    
+
     b0=np.ones((Nreal,NLSS))#find best fit bias for each real, for each LSS tracer
     #print '------reconstructing isw000-------'
     if fitbias:
@@ -324,8 +323,12 @@ def calc_isw_est(cldat,glmdat,recdat,writetofile=True,getmaps=True,redofits=True
             # print ' theory cl for ell=4:',cltheory[4]
             # print ' observ cl for ell=4:',clobs[4,0]#0th real
             # print '       best fit bias:',b0[0,i],b0[0,i]**2
+            # print '    R for ell=4 b0=1:',Dl[4,0,1]/cltheory[4]
+            # print '  R for ell=4 wb0fit:',Dl[4,0,1]/cltheory[4]/b0[0,i]
+
             # plt.figure(0)
             # plt.semilogy(np.arange(Nell),np.fabs(cltheory),label='theory')
+            # plt.semilogy(np.arange(Nell),np.fabs(cltheory*b0[0,i]**2),label='theory*b0^2')
             # plt.semilogy(np.arange(Nell),np.fabs(clobs),label='obs')
             # plt.legend()
             # plt.show()
@@ -349,14 +352,18 @@ def calc_isw_est(cldat,glmdat,recdat,writetofile=True,getmaps=True,redofits=True
     Dlr=np.zeros((Nreal,Nell,NLSS+1,NLSS+1))
     Dinvr=np.zeros((Nreal,Nell,NLSS+1,NLSS+1))
     for r in xrange(Nreal):
+        #print '         bias:',b0[r,:]
         Dlr[r,:,:,:]=scale_Dl_byb0(Dl,b0[r,:]) #b0=1 if no fitting
         Dinvr[r,:,:,:] = invert_Dl(Dlr[r,:,:,:])
-
+        #print '   prescaling:',Dl[4,0,0],Dl[4,0,1],Dl[4,1,1]
+        #print '  postscaling:',Dlr[r,4,0,0],Dlr[r,4,0,1],Dlr[r,4,1,1]
+        #print '  R4 after scaling func',Dlr[r,4,0,1]/Dlr[r,4,1,1]
     #compute estimator; will have same number of realizations as glmdat
     almest=np.zeros((glmgrid.shape[0],1,glmgrid.shape[2]),dtype=np.complex)
     ellvals,emmvals=glmdat.get_hp_landm()
     for lmind in xrange(glmdat.Nlm):
         ell=ellvals[lmind]
+        emm=emmvals[lmind]
         if ell<lmin_forrec or (lmax_forrec>0 and ell>lmax_forrec): 
             continue
         Nl=1./Dinvr[:,ell,0,0] #Nreal sized array
@@ -364,7 +371,6 @@ def calc_isw_est(cldat,glmdat,recdat,writetofile=True,getmaps=True,redofits=True
             almest[:,0,lmind]-=Dinvr[:,ell,0,i+1]*glmgrid[:,i,lmind]
             #print 'just added',-1*Dinv[ell,0,i]*glmgrid[:,i,lmind]
         almest[:,0,lmind]*=Nl
-
     outmaptags=[recdat.maptag]
     outmodtags=[recdat.rectag]
     outmasktags=[recdat.masktag]#for now, only handles changing lmin/max, not masks
@@ -817,12 +823,15 @@ def rho_manyreal(truefilebase,recfilebase,Nreal=1,rlzns=np.array([]),savedat=Fal
     for r in xrange(Nreal):
         f1=''.join([truefilebase,'.r{0:05d}.fits'.format(rlzns[r])])
         f2=''.join([recfilebase,'.r{0:05d}.fits'.format(rlzns[r])])
+        #print f1
+        #print f2
         map1orig=hp.read_map(f1,verbose=False)
         map2orig=hp.read_map(f2,verbose=False)
-        #print ' pre ell removal rms:',np.std(map1orig),np.std(map2orig)
+        #print '  pre ell removal rms:',np.std(map1orig),np.std(map2orig)
         #filter out ell<lmin
         map1=remove_lowell_frommap(map1orig,lmin,lmax)
         map2=remove_lowell_frommap(map2orig,lmin,lmax)
+        #print '  true/rec map rms:',np.std(map1),np.std(map2)
         
         #compute cross correlations and store the value
         if varname=='rho':
@@ -1095,7 +1104,7 @@ def compute_rho_fromcl(cldat,recdat,reccldat=0,varname='rho',fitbias=True):
         if fitbias:
             for i in xrange(NLSS):
                 #only fit to lvalues we want to use
-                b0[i]=fitcl_forb0_onereal(Dl[lmin:lmax+1,i+1,i+1],recDl[lmin:lmax+1,i+1,i+1])
+                b0[i]=fitcl_forb0_onereal(recDl[lmin:lmax+1,i+1,i+1],Dl[lmin:lmax+1,i+1,i+1])
                 #print dtags[i+1],recdtags[i+1],'bias:',b0[i]
             
         recDl=scale_Dl_byb0(recDl,b0)

genMapsfromCor.py
@@ -811,6 +811,7 @@ def get_maps_from_glm_1real(glmdat,rlz=0,redofits=False,makeplots=False,NSIDE=32
 #  outglm - glmdata object with same propreties as dummy, with data filled in
 #------------------------------------------------------------------------
 def getglm_frommaps(dummyglm,rlzns=np.array([]),Nreal=1):
+    print '----------------------in getglm_frmmmaps'
     if rlzns.size:
         Nreal=rlzns.size
     else:
@@ -1132,6 +1133,7 @@ def getmodtag_fixedvar(sig2,shape='g',lmin=0,lmax=30,width=10.):
 # assumes calibration error maps are uncorrelated with each other and galaxies
 #------------------------------------------------------------------------
 def apply_additive_caliberror_tocl(cldat,mapmodcombos=[]):
+    #print '  in apply caliberror to cl'
     Nmap=cldat.Nmap
     Nell=cldat.Nell
     
@@ -1163,6 +1165,7 @@ def apply_additive_caliberror_tocl(cldat,mapmodcombos=[]):
             #put this cal cl into clcal grid
             thisNell=thiscalcl.size
             calcl[i,:thisNell]=thiscalcl
+            #print '    calcl[l=4]=',calcl[i,4]
 
     #epsilon parameter tells us how nbar changes; includign only c00 contrib
     epsilon=calcl[:,0]/np.sqrt(4*np.pi) #is zero if no Clcal input
@@ -1180,9 +1183,10 @@ def apply_additive_caliberror_tocl(cldat,mapmodcombos=[]):
         i,j=crosspairs[n]
         if i==j:
             outcl[n,:]+=calcl[i,:] #additive power from calib error auto power
-        outcl[n,0]+=-1*calcl[i,0]*calcl[j,0] #from some of the epsilon terms 
+        outcl[n,0]+=-1*np.sqrt(calcl[i,0]*calcl[j,0]) #from some of the epsilon terms 
         outcl[n,:]/=(1.+epsilon[i])*(1.+epsilon[j]) #no mod if epsilon small
         #print '  changed?',np.any(outcl[n,:]==cldat.cl[n,:])
+        #print '      ij=',i,j,', outcl[ij,4]=',outcl[n,4],'  prev',cldat.cl[n,4]
         
     #creat outcldata object with new outcl and nbar
     #print 'HAS CL changed? ',np.any(cldat.cl-outcl)

genplots_forpaper.py
@@ -267,6 +267,7 @@ def bintest_plot_rhohist_forpaper(divstr=['6','222','111111']):
 #combine stuff from caltest_compare_clcal_shapes with rhoexpplot
 def caltest_basic_expplot_forpaper(varname='rho',plotdir='output/plots_forpaper/'):
     outname='caltest_'+varname+'_exp_basic.pdf'
+    #outname='caltest_'+varname+'_exp_test.pdf'
     colorlist=['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00']#qualitative
     reclmin=2
     #info about calibration errors
@@ -277,14 +278,15 @@ def caltest_basic_expplot_forpaper(varname='rho',plotdir='output/plots_forpaper/
 
     #what points to plot
     varlist=list(caltest_get_logspaced_varlist(minvar=1.e-8,maxvar=.1,Nperlog=10))
-    shortvarlist=[1.e-7,1.e-6,1.e-5,1.e-4,1.e-3,1.e-2]#for data points 
-
+    shortvarlist=[1.e-7,1.e-6,1.e-5,1.e-4,1.e-3,1.e-2]#for data points
+    
     #get data
     rhoexplist=[]#will be 1D; [variance ] 
     rhoexplist=caltest_get_rhoexp(varlist,lmax=lmaxcal,lmin=lmincal,shape=shape,width=width,overwrite=False,doplot=False,saverho=True,varname=varname,reclmin=reclmin)
     #shapestr=r'$C_{{\ell}}^{{\rm cal}}\propto e^{{-(\ell/{0:.0f})^2}}$'.format(width,lmincal,lmaxcal)
     linelabel='Theory (add. only)'
-    datplot=caltest_getdataplot_forshapecompare(varname,shortvarlist,[shape],[width],[lmincal],[lmaxcal],recminelllist=np.array([reclmin]),colorlist=colorlist,labellist=['Results from sim.'])
+    datplot=caltest_getdataplot_forshapecompare(varname,shortvarlist,[shape],[width],[lmincal],[lmaxcal],recminelllist=np.array([reclmin]),colorlist=colorlist,labellist=['Results from sim.'],getunmod=True)
+    #setting getunmod=false since we don't need cal=0 reference means
 
     #do plotting!
     #assuming last entry in varlist, rhoarray is fiducial (var=0)
@@ -310,8 +312,8 @@ def caltest_basic_expplot_forpaper(varname='rho',plotdir='output/plots_forpaper/
         ax1.set_ylabel(r'$\langle \rho \rangle$')
     elif varname=='s':
         ax1.set_ylabel(r'$\langle s \rangle$')
-        ax1.set_ylim(.1,1.e4)
-        ax1.set_yscale('log')
+        #ax1.set_ylim(.1,1.e4)
+        #ax1.set_yscale('log')
 
     #fill in regions for current/future values
     nowmin=1.e-4
@@ -510,16 +512,20 @@ if __name__=="__main__":
     shortnbarsr=shortnbarlist*((180.*60./np.pi)**2)
     scaletovar=shortnbarsr[0]
     nbarlist=caltest_get_logspaced_varlist(1.e-6,1.e3)
-    shottest_plot_rhoexp(nbarlist=nbarlist,varname='rho',passnbarunit='amin2',overwrite=0,dodata=True,datnbar=shortnbarlist,plotdir='output/plots_forpaper/')
-    shottest_plot_rhoexp(nbarlist=nbarlist,varname='s',passnbarunit='amin2',overwrite=0,dodata=True,datnbar=shortnbarlist,plotdir='output/plots_forpaper/')
-    
-    #z0test_onesim_plot(varname='rho',dohatch=False,plotdir='output/plots_forpaper/')
-    #z0test_onesim_plot(varname='s',dohatch=False,plotdir='output/plots_forpaper/')
-    #bztest_onesim_plot(varname='rho',dohatch=False,plotdir='output/plots_forpaper/') 
-    #bztest_onesim_plot(varname='s',dohatch=False,plotdir='output/plots_forpaper/')
-    badfracs=np.array([0.,1.e-3,5.e-3,1.e-2,2.e-2,5.e-2,.1,.2])
-    #catztest_onesim_plot(varname='rho',Nbins=1,recf=badfracs,fidf=.01,secondfidf=.1,dohatch=False,plotdir='output/plots_forpaper/')
-    #catztest_onesim_plot(varname='s',Nbins=1,recf=badfracs,fidf=.01,secondfidf=.1,dohatch=False,plotdir='output/plots_forpaper/')
+    #shottest_plot_rhoexp(nbarlist=nbarlist,varname='rho',passnbarunit='amin2',overwrite=0,dodata=True,datnbar=shortnbarlist,plotdir='output/plots_forpaper/')
+    #shottest_plot_rhoexp(nbarlist=nbarlist,varname='s',passnbarunit='amin2',overwrite=0,dodata=True,datnbar=shortnbarlist,plotdir='output/plots_forpaper/')
+
+    overwritedat=1
+    # z0test_onesim_plot(varname='rho',dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat)
+    # z0test_onesim_plot(varname='s',dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat)
+    # bztest_onesim_plot(varname='rho',dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat) 
+    # bztest_onesim_plot(varname='s',dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat)
+    # badfracs=np.array([0.,1.e-3,5.e-3,1.e-2,2.e-2,5.e-2,.1,.2])
+    # catztest_onesim_plot(varname='rho',Nbins=1,recf=badfracs,fidf=.01,secondfidf=.1,dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat)
+    # catztest_onesim_plot(varname='s',Nbins=1,recf=badfracs,fidf=.01,secondfidf=.1,dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat)
+    # badfracs=np.array([0.,1.e-3,1.e-2,.1,.2])
+    # catztest_onesim_plot(varname='rho',Nbins=3,recf=badfracs,fidf=.01,secondfidf=.1,dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat)
+    # catztest_onesim_plot(varname='s',Nbins=3,recf=badfracs,fidf=.01,secondfidf=.1,dohatch=False,plotdir='output/plots_forpaper/',overwritedat=overwritedat)
 
     #z0test_Clcomp(perrors=np.array([10,50]),plotdir='output/plots_forpaper/',plotISWgalratio=False)
     #bztest_Clcomp(b2vals=np.array([0.,.1,.5,1.]),plotdir='output/plots_forpaper/',plotISWgalratio=False)